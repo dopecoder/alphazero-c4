4de83482714586e43005b22ae2e62522c57c7da9
Nithin Rao <nramarao@hawk.iit.edu>
2021-05-08 16:04:30 -0500

    Updated checkpoint dir

--- main.py
+++ main.py
@@ -9,10 +9,11 @@ from utils import *
 
 log = logging.getLogger(__name__)
 
-coloredlogs.install(level='INFO')  # Change this to DEBUG to see more info.
-
+coloredlogs.install(level='DEBUG')  # Change this to DEBUG to see more info.
+from google.colab import drive
+drive.mount('/content/gdrive')
 args = dotdict({
-    'numIters': 1000,
+    'numIters': 50,
     'numEps': 100,              # Number of complete self-play games to simulate during a new iteration.
     'tempThreshold': 15,        #
     'updateThreshold': 0.6,     # During arena playoff, new neural net will be accepted if threshold or more of games are won.
@@ -21,7 +22,7 @@ args = dotdict({
     'arenaCompare': 40,         # Number of games to play during arena play to determine if new net will be accepted.
     'cpuct': 1,
 
-    'checkpoint': './temp/',
+    'checkpoint': '/content/gdrive/azc4',
     'load_model': False,
     'load_folder_file': ('/dev/models/8x100x50','best.pth.tar'),
     'numItersForTrainExamplesHistory': 20,
