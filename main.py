8eb267918a74501c5a659fc0eed3c1d1a25c8bc8
Nithin Rao <nramarao@hawk.iit.edu>
2021-05-08 18:16:39 -0500

    Use Best model.

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ args = dotdict({
 
     'checkpoint': './temp/',
     'load_model': True,
-    'load_folder_file': ('./prev_temp/','checkpoint_21.pth.tar'),
+    'load_folder_file': ('./prev_temp/','best.pth.tar'),
     'numItersForTrainExamplesHistory': 20,
 
 })
