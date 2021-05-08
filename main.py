d5e88b7779163d0f2283a70a2b9edbc66eb878dc
Nithin Rao <nramarao@hawk.iit.edu>
2021-05-08 16:58:44 -0500

    Changed checkpoint file.

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ args = dotdict({
 
     'checkpoint': './temp/',
     'load_model': True,
-    'load_folder_file': ('./temp/','best.pth.tar'),
+    'load_folder_file': ('./temp/','checkpoint_23.pth.tar'),
     'numItersForTrainExamplesHistory': 20,
 
 })
