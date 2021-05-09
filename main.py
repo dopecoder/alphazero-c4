7a8bc8377619122552dee168c20fbe13ca26688b
Nithin Rao <nramarao@hawk.iit.edu>
2021-05-09 00:04:37 -0500

    Remove google drive.

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ args = dotdict({
     'arenaCompare': 40,         # Number of games to play during arena play to determine if new net will be accepted.
     'cpuct': 1,
 
-    'checkpoint': './content/gdrive/My Drive/azc4',
+    'checkpoint': './temp/',
     'load_model': False,
     'load_folder_file': ('./prev_temp/','best.pth.tar'),
     'numItersForTrainExamplesHistory': 20,
