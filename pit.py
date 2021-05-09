fe120d70d8f09d9c5154577e8660cdcc77bc5fbe
Nithin Rao <nramarao@hawk.iit.edu>
2021-05-09 10:48:16 -0500

    pit updated.

--- pit.py
+++ pit.py
@@ -14,6 +14,7 @@ any agent.
 """
 
 human_vs_cpu = False
+step_vs_cpu = True
 g = Connect4Game()
 
 # all players
@@ -26,16 +27,18 @@ hp = HumanConnect4Player(g).play
 # nnet players
 n1 = NNet(g)
 n1.load_checkpoint('./temp/','best.pth.tar')
-args1 = dotdict({'numMCTSSims': 50, 'cpuct':1.0})
+args1 = dotdict({'numMCTSSims': 100, 'cpuct':1.0})
 mcts1 = MCTS(g, n1, args1)
 n1p = lambda x: np.argmax(mcts1.getActionProb(x, temp=0))
 
 if human_vs_cpu:
     player2 = hp
+elif step_vs_cpu:
+    player2 = gp
 else:
     n2 = NNet(g)
     n2.load_checkpoint('./temp/', 'checkpoint_10.pth.tar')
-    args2 = dotdict({'numMCTSSims': 50, 'cpuct': 1.0})
+    args2 = dotdict({'numMCTSSims': 100, 'cpuct': 1.0})
     mcts2 = MCTS(g, n2, args2)
     n2p = lambda x: np.argmax(mcts2.getActionProb(x, temp=0))
 
